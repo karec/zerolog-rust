bc326343a59b65f511e5aa5d43e5d94c8e61fedb
evalette <emmanuel.valette@milibris.com>
2016-10-06 16:49:37 +0200

    some refatorisation for main

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,40 @@
+extern crate zmq;
+
 pub mod utils;
+
+
+use utils::zmqdevice::SimpleDevice;
+
+
+pub fn start_forwarder(in_address: &str, out_address: &str, topic: &str) {
+    let mut ctx = zmq::Context::new();
+
+    let mut in_socket = match ctx.socket(zmq::SUB) {
+        Ok(in_socket) => { in_socket },
+        Err(e) => { panic!("Cannot create in_socket for forwarder. Error : {:?}", e)}
+    };
+
+    match in_socket.set_subscribe(topic.as_bytes()) {
+        Ok(()) => (),
+        Err(e) => panic!("Cannot subscribe to topic. Error : {:?}", e)
+    }
+
+    match in_socket.bind(in_address) {
+        Ok(()) => (),
+        Err(e) => panic!("Cannot bind in socket. Error : {:?}", e)
+    }
+
+    let mut out_socket = match ctx.socket(zmq::PUB) {
+        Ok(out_socket) => { out_socket },
+        Err(e) => { panic!(e) }
+    };
+
+    match out_socket.bind(out_address) {
+        Ok(()) => (),
+        Err(e) => panic!("Cannot bind out socket. Error: {:?}", e)
+    }
+
+    let mut device: SimpleDevice = SimpleDevice::new(in_socket, out_socket);
+    println!("Starting forwarder.\nIn socket binded on {}\nOut socket binded on {}", in_address, out_address);
+    device.run()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,9 @@
 extern crate zerolog;
 extern crate zmq;
 
-use zerolog::utils::zmqdevice::SimpleDevice;
+use zerolog::start_forwarder;
 
 fn main() {
-    println!("Hello, world!");
-
-
-    let mut ctx = zmq::Context::new();
-
-    let mut in_socket = match ctx.socket(zmq::SUB) {
-        Ok(in_socket) => { in_socket },
-        Err(e) => { panic!(e) }
-    };
-
-    match in_socket.set_subscribe("".as_bytes()) {
-        Ok(()) => (),
-        Err(e) => panic!("Cannot subscribe to channel : {:?}", e)
-    }
-
-    match in_socket.bind("tcp://*:9001") {
-        Ok(()) => (),
-        Err(e) => panic!("Cannot bind in socket ! Error is {:?}", e)
-    }
-
-    let mut out_socket = match ctx.socket(zmq::PUB) {
-        Ok(out_socket) => { out_socket },
-        Err(e) => { panic!(e) }
-    };
-
-    match out_socket.bind("tcp://*:9002") {
-        Ok(()) => (),
-        Err(e) => panic!("Cannot bind out socket ! Error is {:?}", e)
-    }
-
-    let mut test: SimpleDevice = SimpleDevice::new(in_socket, out_socket);
-    test.run()
+    println!("Starting forwarder");
+    start_forwarder("tcp://*:9001", "tcp://*:9002", "");
 }
